7c3b3a6cc3c0c43c235fccea95aff2f68fa68395
Frey Mansikkaniemi <me@frey.hk>
2020-06-21 22:20:07 +0800

    debug

--- events.js
+++ events.js
@@ -114,7 +114,7 @@ const getEvents = (timedata,sqldata,id,timeZone="UTC",start=null,end=null) => {
             const event = events[key];
             try{
               const distanceCalculation = await timedata.get.distance(id,timeZone,{startDate: new Date(event.start).toISOString(), endDate: new Date(event.end).toISOString()})
-              console.log(distanceCalculation)
+              console.log(Array.isArray(distanceCalculation) ? distanceCalculation[0] : "No array")
               events[key].distance = distanceCalculation.length > 0 ? distanceCalculation[0].time.integral : 0;
             }catch( e ){
               reject(e)
